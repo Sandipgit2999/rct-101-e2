6f7de1abb8fb68c132422000c0e79d2d341a2e5a
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-05-30 16:48:31 +0530

    Product app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Products from "./components/Products";
+import { Container } from "@chakra-ui/react";
+import { Stack, HStack, VStack } from "@chakra-ui/react";
+import AddProduct from "./components/AddProduct";
 
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return (
+    <div>
+      <Products />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { Stack, Image, Text,Tag,TagLabel,Heading,Box } from "@chakra-ui/react";
 
 const Product = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+  // const Text = () => <div />;
+  // const Image = () => <div />;
+  // const Box = () => <div />;
+  // const Stack = () => <div />;
+  // const Heading = () => <div />;
+  // const Tag = () => <div />;
+  // const TagLabel = () => <div />;
   return (
     <Stack data-cy="product">
       <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
+      <Text data-cy="product-category">asdf</Text>
       <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+        <TagLabel data-cy="product-gender">male</TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      <Heading data-cy="product-title">shirt</Heading>
+      <Box data-cy="product-price">dsgdh</Box>
     </Stack>
   );
 };
